df0b7251b1761ea71dcab90c463d3127ae13ed94
alphaWave <alphaWave@gmx.net>
2022-01-09 20:39:32 +0100

    fresh repo

---
+++ Project1/components/LEDStrip/LEDStrip.cpp
@@ -0,0 +1,73 @@
+#include "LEDStrip.h"
+#include "../Adafruit_DotStar/include/Adafruit_DotStar.h"
+
+
+// DATAPIN and CLOCKPIN can usually be any two pins
+enum
+{
+    NUMPIXELS = 60,
+    DATAPIN = 4,
+    CLOCKPIN = 5,
+    MAX_BRIGHTNESS = 255
+};
+
+// This is how we declare a DotStar-object
+Adafruit_DotStar myStrip(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
+// The last parameter is optional -- this is the color data order of the
+// DotStar strip, which has changed over time in different production runs.
+// Your code just uses R,G,B colors, the library then reassigns as needed.
+// Default is DOTSTAR_BRG, so change this if you have an earlier strip.
+
+// Hardware SPI is a little faster, but must be wired to specific pins
+// (Arduino Uno = pin 11 for data, 13 for clock, other boards are different).
+//Adafruit_DotStar strip(NUMPIXELS, DOTSTAR_BRG);
+
+void ledRunning(bool isColored)
+{
+    // Runs 10 LEDs at a time along strip, cycling through red, green and blue.
+    // This requires about 200 mA for all the 'on' pixels + 1 mA per 'off' pixel.
+
+    int head  = 0;
+    int tail = -10;     // Index of first 'on' and 'off' pixels
+    int brightness = 10;
+    uint32_t color = 0xFF0000;      // 'On' color (starts red)
+  
+    while (1)
+    {
+	if (isColored)
+	{
+	    myStrip.setPixelColor(head, color);    // 'On' pixel at head
+	}
+	else
+	{
+	    myStrip.setPixelColor(head, 0xFFFFFF);  // For white pixels only (makes nested if-statement superfluous)
+	}
+	
+	myStrip.setBrightness(brightness);     // adjust brightness between 0 and 255
+	myStrip.setPixelColor(tail, 0);        // 'Off' pixel at tail
+	myStrip.show();                        // Refresh strip
+	delay(20);                             // Pause 20 milliseconds (~50 FPS)
+
+	if(++head >= NUMPIXELS) {         // Increment head index.  Off end of strip?
+	    head = 0;                       //  Yes, reset head index to start
+	    if(isColored && ((color >>= 8) == 0))         //  Next color (R->G->B) ... past blue now?
+	    {
+		color = 0xFF0000;             //   Yes, reset to red
+	    }
+	}
+	if (++tail >= NUMPIXELS)
+	{
+	    tail = 0; // Increment, reset tail index
+	}
+	if((0 == head) && ((brightness = brightness + 10)  > MAX_BRIGHTNESS))
+	{
+	    brightness = 1;
+	}
+    }
+}
+
+
+void ledBlinking(void)
+{
+    
+}
---
+++ Project1/components/LEDStrip/include/LEDStrip.h
@@ -0,0 +1,5 @@
+// All functions pertaining to how the LED-strip is being actuated
+
+
+void ledRunning(bool isColored);
+void ledBlinking(void);
